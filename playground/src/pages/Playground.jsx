931197ffa24f0ea3fdb64a23cef2c20283fbb6fb
1ilit <lilitmousheghyan@gmail.com>
2025-01-09 12:19:12 +0400

    add favicon

--- playground/src/pages/Playground.jsx
+++ playground/src/pages/Playground.jsx
@@ -1,11 +1,15 @@
 import ASTViewer from "../components/ast-viewer/ASTViewer";
+import Header from "../components/header/Header";
 import SQLEditor from "../components/sql-editor/SQLEditor";
 
 export default function Playground() {
   return (
     <div>
-      <SQLEditor />
-      <ASTViewer />
+      <Header />
+      <div className="flex">
+        <SQLEditor />
+        <ASTViewer />
+      </div>
     </div>
   );
 }
